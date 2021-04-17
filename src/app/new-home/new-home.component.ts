01306ee2dfe8c0c749dfabfa666fd6ec56fccf1f
sharmadotankit <sharma.ankit.kamal@gmail.com>
2021-04-17 22:18:04 +0530

    Try Catch added in slide

--- src/app/new-home/new-home.component.ts
+++ src/app/new-home/new-home.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
-import {Observable} from 'rxjs';
-import {startWith, map} from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-home',
@@ -13,36 +13,39 @@ export class NewHomeComponent implements OnInit {
   control = new FormControl();
   streets: string[] = ['Champs-Élysées', 'Lombard Street', 'Abbey Road', 'Fifth Avenue'];
   filteredStreets: Observable<string[]>;
-  source:String="";
-  destination:String="";
+  source: String = "";
+  destination: String = "";
   constructor() { }
   ngOnInit() {
-    
-
     var myIndex = 0;
-carousel();
+    carousel();
 
-function carousel() {
-  var i;
-  var x ;
-  x= document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    function carousel() {
+      var i;
+      var x;
+      x = document.getElementsByClassName("mySlides");
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+      }
+      myIndex++;
+      if (myIndex > x.length) { myIndex = 1 }
+      try{
+        x[myIndex - 1].style.display = "block";
+      }
+      catch{
+        
+      }
+     finally{
+      setTimeout(carousel, 2000); // Change image every 2 seconds
+     }
+     
+    }
+
+
   }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+
+
+
 }
 
-    
-  }
 
-
-  
-}
-
-//   ngOnInit(): void {
-//   }
-
-// }
--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-payment',
@@ -10,9 +10,9 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 export class PaymentComponent implements OnInit {
 
-   constructor() { }
-  
-  
+  constructor() { }
+
+
 
   ngOnInit() {
     var acc = document.getElementsByClassName("accordion");
@@ -29,37 +29,32 @@ export class PaymentComponent implements OnInit {
         }
       });
     }
-      // Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+    // Get the modal
+    var modal = document.getElementById("myModal");
 
-// Get the <span> element that closes the modal
-var span;
-span = document.getElementsByClassName("close")[0];
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+    // Get the <span> element that closes the modal
+    var span;
+    span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+    // When the user clicks the button, open the modal 
+    btn.onclick = function () {
+      modal.style.display = "block";
+    }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
   }
 }
-
-
-
-  }
-
-
-  
-  }
