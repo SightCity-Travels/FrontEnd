1ecd09bea86ac6bf8c0a8b31f4165fb064cca360
PratikshaBSA <acharya.p1998@gmail.com>
2021-04-19 18:19:33 +0530

    input validation span and footer

--- src/app/passenger-details/passenger-details.component.ts
+++ src/app/passenger-details/passenger-details.component.ts
@@ -1,37 +1,67 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
-import { passenger } from '../passenger';
-import { User } from '../User';
 
-@Component({
-  selector: 'app-passenger-details',
-  templateUrl: './passenger-details.component.html',
-  styleUrls: ['./passenger-details.component.css']
-})
-export class PassengerDetailsComponent implements OnInit {
-  user:User=new User();
-  fetchedSeatInfo;
-  passengers:passenger[];
-  numberOfPassengers:number;
-  constructor(private router:Router) { 
-  } 
-  
-  ngOnInit(): void {
-    this.fetchedSeatInfo=JSON.parse(localStorage.getItem("seatsOfPassengers"));
-    this.numberOfPassengers=this.fetchedSeatInfo.length;
+    
 
-  }
-
-  
-
-  passengerDetails(passenger:NgForm){
-    if(passenger.valid){
-   this.router.navigate(['/paymentLink']);
+import {​​​​​​​​ Component, OnInit }​​​​​​​​ from'@angular/core';
+import {​​​​​​​​ NgForm }​​​​​​​​ from'@angular/forms';
+import {​​​​​​​​ Router }​​​​​​​​ from'@angular/router';
+import {​​​​​​​​ passenger }​​​​​​​​ from'../passenger';
+import {​​​​​​​​ User }​​​​​​​​ from'../User';
  
-    }else{
-      alert("Please enter correct information.");
-    }
-  }
+@Component({​​​​​​​​
+selector:'app-passenger-details',
+templateUrl:'./passenger-details.component.html',
+styleUrls: ['./passenger-details.component.css']
+}​​​​​​​​)
+export class PassengerDetailsComponent implements OnInit {​​​​​​​​
+user:User=new User();
+allPassengerList:passenger[]=[];
+fetchedSeatInfo;
+ 
+passengerDetail:passenger= new passenger();
+numberOfPassengers:number;
+constructor(private router:Router) {​​​​​​​​ 
+  }​​​​​​​​ 
 
-}
+ngOnInit(): void {​​​​​​​​
+this.fetchedSeatInfo=JSON.parse(localStorage.getItem("seatsOfPassengers"));
+this.numberOfPassengers=this.fetchedSeatInfo.length;
+for(let i=0;i<this.numberOfPassengers;i++){​​​​​​​​
+console.log(this.fetchedSeatInfo[i]);
+    }​​​​​​​​
+ 
+  }​​​​​​​​
+ 
+addPassenger(){​​​​​​​​
+var  pass:passenger={​​​​​​​​
+passengerId:null,
+passengerName:this.passengerDetail.passengerName,
+passengerAge:this.passengerDetail.passengerAge,
+gender:this.passengerDetail.gender,
+seatNo:this.fetchedSeatInfo[this.numberOfPassengers-1]
+    }​​​​​​​​
+
+this.allPassengerList.push(pass);
+ 
+for(let i=0;i<this.allPassengerList.length;i++){​​​​​​​​
+console.log(this.allPassengerList[i]);
+    }​​​​​​​​
+ 
+this.numberOfPassengers--;
+  }​​​​​​​​
+
+ 
+displayPassenger(){​​​​​​​​
+console.log(this.passengerDetail);
+  }​​​​​​​​
+ 
+passengerDetails(passenger:NgForm){​​​​​​​​
+if(passenger.valid){​​​​​​​​
+this.router.navigate(['/paymentLink']);
+
+    }​​​​​​​​else{​​​​​​​​
+alert("Please enter correct information.");
+    }​​​​​​​​
+  }​​​​​​​​
+ 
+}​​​​​​​​
+
